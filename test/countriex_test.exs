fe315906d675a8d0102806a0065355ba8d38e7b2
mplibunao <markpaololibunao@gmail.com>
2018-10-06 21:51:11 +0800

    Adds tests for get_by and filter functions

--- test/countriex_test.exs
+++ test/countriex_test.exs
@@ -10,6 +10,37 @@ defmodule CountriexTest do
     end
   end
 
+  describe "get_by/2" do
+    test "returns the first country to match the given criteria" do
+      country = Countriex.get_by(:name, "Jamaica")
+
+      assert country.name == "Jamaica"
+    end
+
+    test "returns nil if no country matches the given criteria" do
+      country = Countriex.get_by(:name, "United Kingdom")
+
+      assert country == nil
+    end
+  end
+
+  describe "filter/2" do
+    test "Returns all countries matching the given criteria" do
+      countries = Countriex.filter(:currency_code, "USD")
+      country = List.first(countries)
+
+      assert length(countries) == 20
+      assert country.name == "Antarctica"
+      assert country.currency_code == "USD"
+    end
+
+    test "Returns an empty list if the criteria does not match any countries" do
+      countries = Countriex.filter(:current_code, "foo")
+
+      assert countries == []
+    end
+  end
+
   describe "all_states/0" do
     test "returns all states" do
       states = Countriex.all_states
